c3915da0bba282801caca64af12d0e9142845dc6
Kuncheria-Kuruvilla <kuncheria94@gmail.com>
2023-04-10 13:58:26 +0530

    Add Roses fancy text

---
+++ src/lib/Roses/Roses.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Roses from "./Roses";
+
+const meta: Meta<typeof Roses> = {
+  title: "Component/Roses",
+  component: Roses,
+  parameters: {
+    backgrounds: {
+      default: "Default",
+      values: [
+        { name: "Default", value: "#d52e3f" },
+        { name: "Dark", value: "#242424" },
+      ],
+    },
+  },
+};
+
+export default meta;
+
+export const Primary: StoryObj<typeof Roses> = {
+  args: {
+    text: "Roses",
+  },
+};
